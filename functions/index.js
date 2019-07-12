c17e5f0004ecf2959593f5762a07f6312d6a2d6b
free4mOriginal <free4m@gmail.com>
2019-07-12 14:41:24 -0500

    create getPosts function to fetch user posts

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +9,15 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Tyro Dev!");
 });
+
+exports.getPosts = functions.https.onRequest((req, res) => {
+    admin.firestore().collection('posts').get()
+        .then(data => {
+            let posts = [];
+            data.forEach(doc => {
+                posts.push(doc.data());
+            });
+            return res.json(posts);
+        })
+        .catch(err => console.error(err));
+})
