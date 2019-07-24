40cc0fc850ec3b27edfc873f3aa1301305d5e14c
Rusty Hoppins <rustiphyde@gmail.com>
2019-07-24 10:59:45 -0500

    Add merge conflict attempt

--- tyro-react-app/src/pages/login.js
+++ tyro-react-app/src/pages/login.js
@@ -4,7 +4,7 @@ export class Login extends Component {
     render() {
         return (
             <div>
-                <h1>Login</h1>
+                <h1>Instead of Login just add something...Rusty</h1>
             </div>
         )
     }
