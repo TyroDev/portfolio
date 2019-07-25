3f5cab3f30077cbbf2637b84810c9dd716b0b1ad
free4mOriginal <free4m@gmail.com>
2019-07-25 15:10:30 -0500

    change Mui theme to match ours

--- tyro-react-app/src/App.js
+++ tyro-react-app/src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 
 // Components:
 import Navbar from "./components/Navbar";
@@ -10,21 +12,42 @@ import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={home} />
-            <Route exact path="/login" component={login} />
-            <Route exact path="/signup" component={signup} />
-          </Switch>
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#fbfdf7",
+      main: "#a6ce39",
+      dark: "#425216",
+      contrastText: "#ffffff"
+    },
+    secondary: {
+      light: "#fefce5",
+      main: "#fd9900",
+      dark: "#975b00",
+      contrastText: "#ffffff"
+    }
+  }
+});
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Router>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={home} />
+                <Route exact path="/login" component={login} />
+                <Route exact path="/signup" component={signup} />
+              </Switch>
+            </div>
+          </Router>
         </div>
-      </Router>
-    </div>
-  );
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
