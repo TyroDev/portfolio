c08aff1a04d3d3c48a5e6e74a9b2896726ffc168
free4mOriginal <free4m@gmail.com>
2019-07-29 14:12:18 -0500

    create types.js

--- tyro-react-app/src/App.js
+++ tyro-react-app/src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
-import Logo from "./components/Logo";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import jwtDecode from "jwt-decode";
 import AuthRoute from "./util/AuthRoute";
 
+// Redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 // Components:
 import Navbar from "./components/Navbar";
+import Logo from "./components/Logo";
+import Credits from "./components/Credits";
 
 // Pages:
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
-
-import Credits from "./components/Credits";
 
 let authenticated;
 const token = localStorage.FBIdToken;
@@ -30,30 +33,30 @@ if (token) {
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Router>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={home} />
-              <AuthRoute
-                exact
-                path="/login"
-                component={login}
-                authenticated={authenticated}
-              />
-              <AuthRoute
-                exact
-                path="/signup"
-                component={signup}
-                authenticated={authenticated}
-              />
-            </Switch>
-          </div>
-        </Router>
-        <Logo />
-        <Credits />
-      </div>
+      <Provider store={store}>
+          <Router>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={home} />
+                <AuthRoute
+                  exact
+                  path="/login"
+                  component={login}
+                  authenticated={authenticated}
+                />
+                <AuthRoute
+                  exact
+                  path="/signup"
+                  component={signup}
+                  authenticated={authenticated}
+                />
+              </Switch>
+            </div>
+          </Router>
+          <Logo />
+          <Credits />
+      </Provider>
     );
   }
 }
